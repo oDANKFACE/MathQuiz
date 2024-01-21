f3dcca3602f9d5bfb87ac8d0348cf63d2c9bb5e7
Darien Weller <darien.r.weller@gmail.com>
2024-01-20 18:27:09 -0700

    Quick fix

--- MathQuiz/Form1.cs
+++ MathQuiz/Form1.cs
@@ -92,8 +92,6 @@ namespace MathQuiz
             }
             else
             {
-                // If the user ran out of time, stop the timer, show
-                // a MessageBox, and fill in the answers.
                 timer1.Stop();
                 timeLabel.Text = "Time's up!";
                 MessageBox.Show("You didn't finish in time.", "Sorry!");
